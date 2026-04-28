9a48f5df4eeaecb949c0e8fc5d0f21f619c99461
ingunntonetta <erdalingunn@gmail.com>
2026-04-28 18:32:43 +0200

    creating the board component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import "./App.css";
+import questionsData from './questions.json';
+import Board from './components/Board';
 
 function App() {
   return (
-    <div className="card">
-      <h1>Velkommen! 👋</h1>
-      <p>
-        Du kan se koden for dette prosjektet{" "}
-        <a href="https://github.com/bekk/prosjektutgangspunkt">
-          i repoet på GitHub
-        </a>
-      </p>
-      <p>
-        Du kan endre koden i <code>src/App.jsx</code>
-      </p>
-    </div>
+      <div className="game-screen">
+          <h1 className="game-title">🎯 Jeopardy</h1>
+          <Board />
+      </div>
   );
 }
 
